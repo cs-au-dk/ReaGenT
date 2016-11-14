5e6a42e819aa48f88b92fa1f75d7a62866b8322b
Erik Krogh Kristensen <erik@webbies.dk>
2016-11-14 10:57:53 +0100

    async and underscore benchmarks

--- src/dk/webbies/tajscheck/benchmarks/Benchmarks.java
+++ src/dk/webbies/tajscheck/benchmarks/Benchmarks.java
@@ -9,4 +9,8 @@ public class Benchmarks {
     public static Benchmark test = new Benchmark(ParseDeclaration.Environment.ES5Core, "tests/test/module.js", "tests/test/module.d.ts", "moment");
 
     public static Benchmark moment = new Benchmark(ParseDeclaration.Environment.ES5Core, "tests/moment/moment.js", "tests/moment/moment.d.ts", "moment");
+
+    public static Benchmark async = new Benchmark(ParseDeclaration.Environment.ES5Core, "tests/async/async.js", "tests/async/async.d.ts", "async");
+
+    public static Benchmark underscore = new Benchmark(ParseDeclaration.Environment.ES5Core, "tests/underscore/underscore.js", "tests/underscore/underscore.d.ts", "_");
 }
