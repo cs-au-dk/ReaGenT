78d1d8d6b0f6b39083a5a993683e137176fe8fe1
Erik Krogh Kristensen <erik@webbies.dk>
2017-01-10 10:20:58 +0100

    Added a whole lot of test files (benchmarks incomming)

--- test/src/dk/webbies/tajscheck/test/dynamic/RunBenchmarks.java
+++ test/src/dk/webbies/tajscheck/test/dynamic/RunBenchmarks.java
@@ -61,26 +61,31 @@ public class RunBenchmarks {
 
 
         benchmarks.put("ace.js", new Benchmark(ParseDeclaration.Environment.ES5Core, "test/benchmarks/ace/ace.js", "test/benchmarks/ace/ace.d.ts", "ace", BROWSER, options));
-        benchmarks.put("jQuery.js", new Benchmark(ParseDeclaration.Environment.ES5Core, "test/benchmarks/jquery/jquery.js", "test/benchmarks/jquery/jquery.d.ts", "jQuery", BROWSER, options.getBuilder().setIterationsToRun(500).build()));
+        Benchmark jQuery = new Benchmark(ParseDeclaration.Environment.ES5Core, "test/benchmarks/jquery/jquery.js", "test/benchmarks/jquery/jquery.d.ts", "jQuery", BROWSER, options.getBuilder().setIterationsToRun(500).build());
+        benchmarks.put("jQuery.js", jQuery);
 
 
         benchmarks.put("angular.js",
                 new Benchmark(ParseDeclaration.Environment.ES5Core, "test/benchmarks/angular1/angular1.js", "test/benchmarks/angular1/angular1.d.ts", "angular", BROWSER, options)
-                .addDependency(benchmarks.get("jQuery.js"))
+                .addDependencies(benchmarks.get("jQuery.js"))
         );
 
 
         benchmarks.put("box2dweb", new Benchmark(ParseDeclaration.Environment.ES5Core, "test/benchmarks/box2dweb/box2dweb.js", "test/benchmarks/box2dweb/box2dweb.d.ts", "Box2D", BROWSER, options));
 
-        // The TypeScript parser breaks.
+        // The TypeScript parser breaks on the below.
 //        benchmarks.put("createjs", new Benchmark(ParseDeclaration.Environment.ES5Core, "test/benchmarks/createjs/createjs.js", "test/benchmarks/createjs/createjs.d.ts", "createjs", BROWSER, options));
+//        benchmarks.put("vue", new Benchmark(ParseDeclaration.Environment.ES5Core, "test/benchmarks/vue/vue.js", "test/benchmarks/vue/index.d.ts", "Vue", BROWSER, options));
+//        benchmarks.put("three", new Benchmark(ParseDeclaration.Environment.ES5Core, "test/benchmarks/three/three.js", "test/benchmarks/three/three.d.ts", "THREE", BROWSER, options));
+//        benchmarks.put("photoswipe", new Benchmark(ParseDeclaration.Environment.ES5Core, "test/benchmarks/photoswipe/photoswipe.js", "test/benchmarks/photoswipe/photoswipe.d.ts", "PhotoSwipe", BROWSER, options));
+//        benchmarks.put("leaflet", new Benchmark(ParseDeclaration.Environment.ES5Core, "test/benchmarks/leaflet/leaflet.js", "test/benchmarks/leaflet/leaflet.d.ts", "leaflet", BROWSER, options));
 
         benchmarks.put("underscore", new Benchmark(ParseDeclaration.Environment.ES5Core, "test/benchmarks/underscore/underscore.js", "test/benchmarks/underscore/underscore.d.ts", "_", NODE, options));
 
         // Blows up in complexity, not sure why. (Option to disable all generics?)
         /*benchmarks.put("backbone",
                 new Benchmark(ParseDeclaration.Environment.ES5Core, "test/benchmarks/backbone/backbone.js", "test/benchmarks/backbone/backbone.d.ts", "Backbone", BROWSER, options)
-                .addDependency(benchmarks.get("underscore"))
+                .addDependencies(benchmarks.get("underscore"))
         );*/
 
         benchmarks.put("handlebars", new Benchmark(ParseDeclaration.Environment.ES6DOM, "test/benchmarks/handlebars/handlebars.js", "test/benchmarks/handlebars/handlebars.d.ts", "Handlebars", BROWSER, options));
@@ -96,6 +101,32 @@ public class RunBenchmarks {
         benchmarks.put("ember", new Benchmark(ParseDeclaration.Environment.ES5DOM, "test/benchmarks/ember/ember.js", "test/benchmarks/ember/ember.d.ts", "Ember", BROWSER, options));
 
         benchmarks.put("d3", new Benchmark(ParseDeclaration.Environment.ES5Core, "test/benchmarks/d3/d3.js", "test/benchmarks/d3/d3.d.ts", "d3", BROWSER, options));
+
+        // TODO: StackOverflow in FindPositiveTypesVisitor (<- use this to implement some delta-debugger thing).
+        benchmarks.put("lodash", new Benchmark(ParseDeclaration.Environment.ES5Core, "test/benchmarks/lodash/lodash.js", "test/benchmarks/lodash/lodash.d.ts", "_", NODE, options));
+
+        // TODO: Causes the sanityCheck to timeout! (use the delta-debugger made for lodash).
+        benchmarks.put("p2", new Benchmark(ParseDeclaration.Environment.ES5Core, "test/benchmarks/p2/p2.js", "test/benchmarks/p2/p2.d.ts", "p2", BROWSER, options));
+
+        benchmarks.clear();
+
+        Benchmark webcomponents = new Benchmark(ParseDeclaration.Environment.ES5Core, "test/benchmarks/webcomponents/webcomponents.js", "test/benchmarks/webcomponents/webcomponents.d.ts", "webcomponents", BROWSER, options);
+        benchmarks.put("webcomponents", webcomponents);
+
+        benchmarks.put("mathjax", new Benchmark(ParseDeclaration.Environment.ES5Core, "test/benchmarks/mathjax/mathjax.js", "test/benchmarks/mathjax/mathjax.d.ts", "MathJax", BROWSER, options));
+
+        benchmarks.put("peerjs", new Benchmark(ParseDeclaration.Environment.ES5Core, "test/benchmarks/peerjs/peerjs.js", "test/benchmarks/peerjs/peerjs.d.ts", "PeerJs", BROWSER, options));
+        benchmarks.put("pickadate", new Benchmark(ParseDeclaration.Environment.ES5Core, "test/benchmarks/pickadate/picker.js", "test/benchmarks/pickadate/pickadate.d.ts", "Pickadate", BROWSER, options).addDependencies(jQuery));
+        benchmarks.put("pleasejs", new Benchmark(ParseDeclaration.Environment.ES5Core, "test/benchmarks/pleasejs/please.js", "test/benchmarks/pleasejs/please.d.ts", "Please", NODE, options));
+        benchmarks.put("polymer", new Benchmark(ParseDeclaration.Environment.ES5Core, "test/benchmarks/polymer/polymer.js", "test/benchmarks/polymer/polymer.d.ts", "polymer", BROWSER, options).addDependencies(webcomponents));
+        benchmarks.put("q", new Benchmark(ParseDeclaration.Environment.ES5Core, "test/benchmarks/q/q.js", "test/benchmarks/q/q.d.ts", "Q", BROWSER, options));
+        benchmarks.put("qunit", new Benchmark(ParseDeclaration.Environment.ES5Core, "test/benchmarks/qunit/qunit.js", "test/benchmarks/qunit/qunit.d.ts", "QUnit", BROWSER, options));
+        benchmarks.put("react", new Benchmark(ParseDeclaration.Environment.ES5Core, "test/benchmarks/react/react.js", "test/benchmarks/react/react.d.ts", "React", BROWSER, options));
+        benchmarks.put("requirejs", new Benchmark(ParseDeclaration.Environment.ES5Core, "test/benchmarks/requirejs/require.js", "test/benchmarks/requirejs/requirejs.d.ts", "requirejs", BROWSER, options));
+        benchmarks.put("sugar", new Benchmark(ParseDeclaration.Environment.ES5Core, "test/benchmarks/sugar/sugar.js", "test/benchmarks/sugar/sugar.d.ts", "sugarjs", BROWSER, options));
+
+
+
 
     }
 
