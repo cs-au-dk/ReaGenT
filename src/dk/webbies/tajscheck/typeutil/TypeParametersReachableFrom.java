4e7a62c68f994cb1fa167bc9911044578753357c
Erik Krogh Kristensen <erik@webbies.dk>
2017-01-10 23:35:57 +0100

    Only keeping bound type parameters in the TypeContext

--- src/dk/webbies/tajscheck/typeutil/TypeParametersReachableFrom.java
+++ src/dk/webbies/tajscheck/typeutil/TypeParametersReachableFrom.java
@@ -18,14 +18,18 @@ public class TypeParametersReachableFrom {
         MultiMap<Type, TypeParameterType> result = new HashSetMultiMap<>();
 
         for (Type type : allTypes) {
-            type.accept(new FindReachableTypeParameters(type, result));
+            type.accept(new FindReachableTypeParameters(type, result), Collections.emptySet());
         }
 
 
         return result;
     }
 
-    private static final class FindReachableTypeParameters implements TypeVisitor<Void> {
+    // TODO: Use mapped, use it to make sure that nothing that is already mapped by a reference, is added to the set of visible types.
+    // TODO: Maybe rename the resulting map to freeTypeParameters.
+
+    // TODO: Combine thisTypesVisible (and cache it), and freeTypeParameters, into a single class that can answer the questions.
+    private static final class FindReachableTypeParameters implements TypeVisitorWithArgument<Void, Set<TypeParameterType>> {
         final Set<Type> seen = new HashSet<>();
         private Type baseType;
         private MultiMap<Type, TypeParameterType> result;
@@ -36,259 +40,244 @@ public class TypeParametersReachableFrom {
         }
 
         @Override
-        public Void visit(AnonymousType t) {
+        public Void visit(AnonymousType t, Set<TypeParameterType> mapped) {
             return null;
         }
 
         @Override
-        public Void visit(ClassType t) {
+        public Void visit(ClassType t, Set<TypeParameterType> mapped) {
             if (seen.contains(t)) {
                 return null;
             }
             seen.add(t);
-            if (result.containsKey(t)) {
-                result.putAll(baseType, result.get(t));
-                return null;
-            }
+            if (addExisting(t, mapped)) return null;
 
-            t.getInstanceType().accept(this);
+            t.getInstanceType().accept(this, mapped);
 
             for (Signature signature : t.getSignatures()) {
                 assert signature.getResolvedReturnType() == null;
                 for (Signature.Parameter parameter : signature.getParameters()) {
-                    parameter.getType().accept(this);
+                    parameter.getType().accept(this, mapped);
                 }
             }
 
-            t.getBaseTypes().forEach(this::accept);
-            t.getStaticProperties().values().forEach(this::accept);
-            t.getTarget().accept(this);
-            t.getTypeArguments().forEach(this::accept);
+            t.getBaseTypes().forEach(base -> base.accept(this, mapped));
+            t.getStaticProperties().values().forEach(prop -> prop.accept(this, mapped));
+            t.getTarget().accept(this, mapped);
+            t.getTypeArguments().forEach(arg -> arg.accept(this, mapped));
 
             return null;
         }
 
         @Override
-        public Void visit(GenericType t) {
+        public Void visit(GenericType t, Set<TypeParameterType> mapped) {
             if (seen.contains(t)) {
                 return null;
             }
             seen.add(t);
-            if (result.containsKey(t)) {
-                result.putAll(baseType, result.get(t));
-                return null;
-            }
 
-            t.toInterface().accept(this);
+            if (addExisting(t, mapped)) return null;
 
-            t.getTarget().accept(this);
+            t.toInterface().accept(this, mapped);
 
-            t.getTypeArguments().forEach(this::accept);
+            t.getTarget().accept(this, mapped);
+
+            t.getTypeArguments().forEach(arg -> arg.accept(this, mapped));
 
             return null;
         }
 
         @Override
-        public Void visit(InterfaceType t) {
+        public Void visit(InterfaceType t, Set<TypeParameterType> mapped) {
             if (seen.contains(t)) {
                 return null;
             }
             seen.add(t);
-            if (result.containsKey(t)) {
-                result.putAll(baseType, result.get(t));
-                return null;
-            }
+            if (addExisting(t, mapped)) return null;
 
-
-            t.getTypeParameters().forEach(this::accept);
-            t.getBaseTypes().forEach(this::accept);
-            t.getDeclaredProperties().values().forEach(this::accept);
+            t.getTypeParameters().forEach(par -> par.accept(this, mapped));
+            t.getBaseTypes().forEach(base -> base.accept(this, mapped));
+            t.getDeclaredProperties().values().forEach(prop -> prop.accept(this, mapped));
             for (Signature signature : Util.concat(t.getDeclaredConstructSignatures(), t.getDeclaredCallSignatures())) {
-                signature.getResolvedReturnType().accept(this);
-                for (Signature.Parameter parameter : signature.getParameters()) {
-                    parameter.getType().accept(this);
-                }
+                signature.getResolvedReturnType().accept(this, mapped);
+                signature.getParameters().forEach(par -> par.getType().accept(this, mapped));
             }
 
             if (t.getDeclaredNumberIndexType() != null) {
-                t.getDeclaredNumberIndexType().accept(this);
+                t.getDeclaredNumberIndexType().accept(this, mapped);
             }
             if (t.getDeclaredStringIndexType() != null) {
-                t.getDeclaredStringIndexType().accept(this);
+                t.getDeclaredStringIndexType().accept(this, mapped);
             }
 
             return null;
         }
 
-        private void accept(Type type) {
-            type.accept(this);
+        private boolean addExisting(Type t, Set<TypeParameterType> mapped) {
+            if (!result.containsKey(t)) {
+                return false;
+            }
+            for (TypeParameterType parameterType : result.get(t)) {
+                if (!mapped.contains(parameterType)) {
+                    result.put(baseType, parameterType);
+                }
+            }
+
+            return true;
         }
 
         @Override
-        public Void visit(ReferenceType t) {
+        public Void visit(ReferenceType t, Set<TypeParameterType> orgMapped) {
             if (seen.contains(t)) {
                 return null;
             }
             seen.add(t);
-            if (result.containsKey(t)) {
-                result.putAll(baseType, result.get(t));
-                return null;
-            }
 
-            t.getTarget().accept(this);
+            Set<TypeParameterType> mapped = Util.concatSet(orgMapped, Util.cast(TypeParameterType.class, TypesUtil.getTypeParameters(t.getTarget())));
 
-            t.getTypeArguments().forEach(this::accept);
+            if (addExisting(t, mapped)) return null;
+
+            t.getTarget().accept(this, mapped);
+
+            t.getTypeArguments().forEach(arg -> arg.accept(this, mapped));
 
             return null;
         }
 
         @Override
-        public Void visit(SimpleType t) {
+        public Void visit(SimpleType t, Set<TypeParameterType> mapped) {
             return null;
         }
 
         @Override
-        public Void visit(TupleType t) {
+        public Void visit(TupleType t, Set<TypeParameterType> mapped) {
             if (seen.contains(t)) {
                 return null;
             }
             seen.add(t);
-            if (result.containsKey(t)) {
-                result.putAll(baseType, result.get(t));
-                return null;
-            }
+            if (addExisting(t, mapped)) return null;
 
-            t.getElementTypes().forEach(this::accept);
+            t.getElementTypes().forEach(type -> type.accept(this, mapped));
 
             return null;
         }
 
         @SuppressWarnings("Duplicates")
         @Override
-        public Void visit(UnionType t) {
+        public Void visit(UnionType t, Set<TypeParameterType> mapped) {
             if (seen.contains(t)) {
                 return null;
             }
             seen.add(t);
-            if (result.containsKey(t)) {
-                result.putAll(baseType, result.get(t));
-                return null;
-            }
+            if (addExisting(t, mapped)) return null;
 
-            t.getElements().forEach(this::accept);
+            t.getElements().forEach(element -> element.accept(this, mapped));
 
             return null;
         }
 
         @Override
-        public Void visit(UnresolvedType t) {
+        public Void visit(UnresolvedType t, Set<TypeParameterType> mapped) {
             throw new RuntimeException();
         }
 
         @Override
-        public Void visit(TypeParameterType t) {
+        public Void visit(TypeParameterType t, Set<TypeParameterType> mapped) {
             if (seen.contains(t)) {
                 return null;
             }
             seen.add(t);
-            if (result.containsKey(t)) {
-                result.putAll(baseType, result.get(t));
-                return null;
+            if (addExisting(t, mapped)) return null;
+
+
+            if (!mapped.contains(t)) {
+                result.put(baseType, t);
             }
-
-
-            result.put(baseType, t);
 
             if (t.getConstraint() != null) {
-                t.getConstraint().accept(this);
+                t.getConstraint().accept(this, mapped);
             }
 
             return null;
         }
 
         @Override
-        public Void visit(SymbolType t) {
+        public Void visit(SymbolType t, Set<TypeParameterType> mapped) {
             return null;
         }
 
         @Override
-        public Void visit(StringLiteral t) {
+        public Void visit(StringLiteral t, Set<TypeParameterType> mapped) {
             return null;
         }
 
         @Override
-        public Void visit(BooleanLiteral t) {
+        public Void visit(BooleanLiteral t, Set<TypeParameterType> mapped) {
             return null;
         }
 
         @Override
-        public Void visit(NumberLiteral t) {
+        public Void visit(NumberLiteral t, Set<TypeParameterType> mapped) {
             return null;
         }
 
         @SuppressWarnings("Duplicates")
         @Override
-        public Void visit(IntersectionType t) {
+        public Void visit(IntersectionType t, Set<TypeParameterType> mapped) {
             if (seen.contains(t)) {
                 return null;
             }
             seen.add(t);
-            if (result.containsKey(t)) {
-                result.putAll(baseType, result.get(t));
-                return null;
-            }
+            if (addExisting(t, mapped)) return null;
 
-            t.getElements().forEach(this::accept);
+            t.getElements().forEach(element -> element.accept(this, mapped));
 
             return null;
         }
 
         @Override
-        public Void visit(ClassInstanceType t) {
-            t.getClassType().accept(this);
+        public Void visit(ClassInstanceType t, Set<TypeParameterType> mapped) {
+            t.getClassType().accept(this, mapped);
+
+            // TODO: Try to use this thing instead, and see if anything breaks.
+//            ((ClassType)t.getClassType()).getInstanceType().accept(this, mapped);
 
             return null;
         }
 
         @Override
-        public Void visit(NeverType t) {
+        public Void visit(NeverType t, Set<TypeParameterType> mapped) {
             return null;
         }
 
         @Override
-        public Void visit(ThisType t) {
+        public Void visit(ThisType t, Set<TypeParameterType> mapped) {
             if (seen.contains(t)) {
                 return null;
             }
             seen.add(t);
-            if (result.containsKey(t)) {
-                result.putAll(baseType, result.get(t));
-                return null;
-            }
+            if (addExisting(t, mapped)) return null;
 
-            t.getConstraint().accept(this);
+            t.getConstraint().accept(this, mapped);
 
             return null;
         }
 
         @Override
-        public Void visit(IndexType t) {
+        public Void visit(IndexType t, Set<TypeParameterType> mapped) {
             throw new RuntimeException();
         }
 
         @Override
-        public Void visit(IndexedAccessType t) {
+        public Void visit(IndexedAccessType t, Set<TypeParameterType> mapped) {
             if (seen.contains(t)) {
                 return null;
             }
             seen.add(t);
-            if (result.containsKey(t)) {
-                result.putAll(baseType, result.get(t));
-                return null;
-            }
+            if (addExisting(t, mapped)) return null;
 
-            t.getIndexType().accept(this);
-            t.getObjectType().accept(this);
+            t.getIndexType().accept(this, mapped);
+            t.getObjectType().accept(this, mapped);
 
             return null;
         }
