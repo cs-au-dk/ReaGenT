8676e6f50eade3aa34fe95261141fe9be880ae76
Erik Krogh Kristensen <erik@webbies.dk>
2017-05-22 13:35:25 +0200

    Benchmarks shouldn't monitor unknown property accesses pr. default

--- test/src/dk/webbies/tajscheck/test/dynamic/RunBenchmarks.java
+++ test/src/dk/webbies/tajscheck/test/dynamic/RunBenchmarks.java
@@ -42,7 +42,6 @@ public class RunBenchmarks {
     static {
         CheckOptions options = CheckOptions.builder()
                 .setSplitUnions(false) // Because some of these benchmarks use an insane amount of overloads, so this can cause the size of the generated program to explode (about a factor 400x for moment).
-                .setMonitorUnkownPropertyAccesses(true)
                 .build();
 
         register(new Benchmark("Moment.js", ParseDeclaration.Environment.ES5Core, "test/benchmarks/moment/moment.js", "test/benchmarks/moment/moment.d.ts", BROWSER, options));
