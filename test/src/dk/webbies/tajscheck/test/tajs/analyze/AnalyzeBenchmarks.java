89964f2982a398d77a99207d633da11ccdc08eb6
Erik Krogh Kristensen <erik@webbies.dk>
2017-09-22 13:36:52 +0200

    Update benchmarks

--- test/src/dk/webbies/tajscheck/test/tajs/analyze/AnalyzeBenchmarks.java
+++ test/src/dk/webbies/tajscheck/test/tajs/analyze/AnalyzeBenchmarks.java
@@ -59,79 +59,54 @@ public class AnalyzeBenchmarks extends TestCase {
             "PDF.js",
             "Foundation",
             "Materialize",
-            "Backbone.js"
-    ));
-
-
-    // Benchmarks that does not invoke any DOM functions, and are on the whitelist
-    static final Set<String> simpleBenchmarks = new HashSet<>(Arrays.asList(
-            "async",
-            "PleaseJS",
-//            "Moment.js",
-//            "box2dweb",
-            "accounting.js",
-            "lunr.js"
-    ));
-
-    // Benchmarks that for various reasons are unanalyzeable.
-    static final Set<String> blacklist = new HashSet<>(Arrays.asList(
-            // because it has getters/setters, which TAJS does not support
-            "Vue.js",
-            "three.js",
-            "Ember.js",
-            "Polymer", // <- because webcomponents has getter.
-
-            "Ace", // Calls unmodelled Object.freeze
-            "jQuery", // Too imprecise calls to Function
-            "RequireJS", // weird error, replicated in TestMicro
-            "QUnit", // weird error with arrays.
-            "React", // No transfer function for Object.freeze.
-            "axios", // Empty value
-            "Modernizr", // Run a WebGL function that is unsupported. (and sometimes it timeouts)
-            "Hammer.js", // Object.assign crashes TAJS
-            "RxJS", // Unexpected polymorphic value
-            "Redux", // TypeError, call to non-function (DOM): HTMLTextAreaElement constructor
-            "Knockout", // Trying to call toString for Array with redefined join-property.
-            "MathJax" // "Unevalable eval: window"
-    ));
-
-    // Benchmarks where just the initialization reaches a timeout
-    static final Set<String> timeouts = new HashSet<>(Arrays.asList(
-            "Chart.js",
-            "AngularJS",
-            "P2.js",
-            "bluebird",
-            "Fabric.js",
-            "Ionic",
-            "Video.js",
-            "PeerJS",
+            "Backbone.js",
+            "Moment.js",
+            "Medium Editor",
             "CreateJS",
             "Handlebars",
+            "Redux",
+            "QUnit",
+            "Knockout",
+            "axios",
             "D3.js",
-            "Moment.js",
-            "PixiJS",
-            "Lodash",
-            "Sugar", // sometimes within the timeout, but far from always, so it ends up here. 
-            "Underscore.js",
-            "Medium Editor"
+            "PeerJS",
+            "Hammer.js"
     ));
 
-    // Sanity checks on the above lists.
-    static {
-        if (!Util.concat(whitelist, blacklist, timeouts, simpleBenchmarks).stream().allMatch(RunBenchmarks.benchmarks.keySet()::contains)) {
-            System.err.println("AnalyzeBenchmarks: A benchmark was misspelled");
-        }
-        if (!simpleBenchmarks.stream().allMatch(whitelist::contains)) {
-            System.err.println("AnalyzeBenchmarks: A benchmark in SimpleBenchmarks wasn't in the whiteList");
-        }
-        RunBenchmarks.benchmarks.keySet().stream().filter(Util.not(whitelist::contains)).filter(Util.not(blacklist::contains)).filter(Util.not(timeouts::contains)).forEach(forgotten -> System.err.println("AnalyzeBenchmarks: " + forgotten + " was forgotten!"));
-    }
+    static final Set<String> blackList = new HashSet<>(Arrays.asList(
+            "AngularJS",
+            "MathJax",
+            "Chart.js",
+            "PixiJS",
+            "P2.js",
+            "bluebird",
+            "Vue.js",
+            "Modernizr",
+            "Fabric.js",
+            "Video.js",
+            "RequireJS",
+            "Lodash",
+            "Sugar",
+            "Ace",
+            "Ember",
+            "QUnit",
+            "Polymer",
+            "Backbone.js",
+            "React",
+            "Knockout",
+            "async",
+            "q",
+            "jQuery",
+            "Underscore.js",
+            "RxJS",
+            "three.js",
+            "Ember.js",
+            "Ionic"
+    ));
 
     @Parameterized.Parameters(name = "{0}")
     public static List<Benchmark> getBenchmarks() {
         return RunBenchmarks.getBenchmarks().stream()
-//                .filter(bench -> blacklist.contains(bench.name))
-//                .filter(bench -> timeouts.contains(bench.name))
                 .filter(bench -> whitelist.contains(bench.name))
                 .collect(Collectors.toList());
     }
@@ -144,7 +119,7 @@ public class AnalyzeBenchmarks extends TestCase {
                 .build().getBuilder();
     }
 
-    @Test
+    @Test(timeout = 120 * 1000)
     public void analyzeBenchmark() throws Exception {
         Benchmark benchmark = this.benchmark.withOptions(options());
         try {
@@ -154,7 +129,7 @@ public class AnalyzeBenchmarks extends TestCase {
         }
     }
 
-    @Test
+    @Test(timeout = 120 * 1000)
     public void analyzeBenchmarkLimitedSideEffects() throws Exception {
         Benchmark benchmark = this.benchmark.withOptions(options -> options().apply(options).staticOptions.setLimitSideEffects(true));
         try {
@@ -165,11 +140,11 @@ public class AnalyzeBenchmarks extends TestCase {
         }
     }
 
-    @Test
+    @Test(timeout = 90 * 1000)
     public void initialize() throws Exception {
         Benchmark benchmark = this.benchmark.withOptions(options -> options().apply(options).setOnlyInitialize(true));
         try {
-            System.out.println(TAJSUtil.runNoDriver(benchmark, 30));
+            System.out.println(TAJSUtil.runNoDriver(benchmark, 91));
         } catch (TimeoutException ignored) {
             System.err.println("Timeout");
         }
