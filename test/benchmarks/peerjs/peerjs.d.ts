b1fc18989ef54fccba084be3825f1d391a546421
Erik Krogh Kristensen <erik@webbies.dk>
2017-09-22 13:36:20 +0200

    update peerjs benchmark

--- test/benchmarks/peerjs/peerjs.d.ts
+++ test/benchmarks/peerjs/peerjs.d.ts
@@ -3,7 +3,7 @@
 // Definitions by: Toshiya Nakakura <https://github.com/nakakura>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-/// <reference types="webrtc" />
+/// <reference path="webrtc/index.d.ts" />
 
 declare namespace PeerJs{
     interface PeerJSOption{
