afbc2d22601f2e20e7d9196060efdf7520ac8709
Erik Krogh Kristensen <erik@webbies.dk>
2017-11-14 15:24:33 +0100

    Added a fixedExpansionOrder, for testing

---
+++ src/dk/webbies/tajscheck/benchmark/options/staticOptions/FixedExpansionOrder.java
@@ -0,0 +1,26 @@
+package dk.webbies.tajscheck.benchmark.options.staticOptions;
+
+import dk.webbies.tajscheck.testcreator.test.FunctionTest;
+
+import java.util.List;
+
+public class FixedExpansionOrder implements ExpansionPolicy {
+    private final List<String> executionOrder;
+    private int index = 0;
+
+    public FixedExpansionOrder(List<String> executionOrder) {
+        this.executionOrder = executionOrder;
+    }
+
+    @Override
+    public void nextRound() {
+        index++;
+    }
+
+    @Override
+    public boolean include(FunctionTest test) {
+        assert executionOrder.contains(test.getPath());
+        List<String> subList = executionOrder.subList(0, Math.min(index, executionOrder.size()));
+        return subList.contains(test.getPath());
+    }
+}
