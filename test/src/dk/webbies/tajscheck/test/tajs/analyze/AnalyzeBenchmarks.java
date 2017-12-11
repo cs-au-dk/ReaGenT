e887d1050e0b635ebee45bbe32c0bdfd42736988
Erik Krogh Kristensen <erik@webbies.dk>
2017-12-11 09:29:40 +0100

    Comments on some benchmarks, and get a separate "getPatchedBenchmark" function

--- test/src/dk/webbies/tajscheck/test/tajs/analyze/AnalyzeBenchmarks.java
+++ test/src/dk/webbies/tajscheck/test/tajs/analyze/AnalyzeBenchmarks.java
@@ -37,25 +37,26 @@ public class AnalyzeBenchmarks extends TestCase {
     // Benchmarks that seem analyzeable.
     // TODO: Most of these remarks are before the rebase to extended.
     static final Set<String> whitelist = new HashSet<>(Arrays.asList(
-            "async", // can analyze
-            "Moment.js", // can analyze (requires lots of memory)
-            "pathjs", // can analyze.
-            "Zepto.js", // can analyze. (TODO: Try to run with a lot of mem, it after rebase it seems different) (Before: Gets a useless spurious result after few minutes, because: We analyze the global object, is fine, we analyze some methods get some state, doing this a spurious write is performed on the global object, this causes everything except global object to be removed from type-to-test, and the single spurious error is reported.)
-            "reveal.js", // can analyze. But takes a while.
-            "PleaseJS", // can analyze
-            "CodeMirror", // TODO: EVERYTHING was retracted? (even LoadModuleTest)
-            "PhotoSwipe", // encounters (cannot construct intersectionType) at top-level constructor.
-            "Jasmine", // has a lot of globals that it cannot find (because they aren't registered).
-            "Swiper", // Top level constructor gets retracted.
-            "box2dweb", // TODO: Big, takes a lot of mem.
             "Sortable", // can analyze
+            "async", // currently loops/takes to long.
+            "PleaseJS", // can analyze
+            "PhotoSwipe", // encounters (cannot construct intersectionType) at top-level constructor.
+            "Knockout", // 48 violations in the top-level object. So no methods are called.
+            "Swiper", // Top level constructor gets retracted (takes way to long).
+
+            "Moment.js", // can analyze (requires lots of memory)
+            "pathjs", // can analyze. (TODO: infinite loop?)
+            "Zepto.js", // can analyze. (TODO: Try to run with a lot of mem, it after rebase it seems different) (Before: Gets a useless spurious result after few minutes, because: We analyze the global object, is fine, we analyze some methods get some state, doing this a spurious write is performed on the global object, this causes everything except global object to be removed from type-to-test, and the single spurious error is reported.)
+            "reveal.js", // can analyze. But takes a while. (TODO: Java crashed while running it last time, likely out-of-mem).
+            "CodeMirror", // TODO: EVERYTHING was retracted? (even LoadModuleTest)
+            "Jasmine", // has a lot of globals that it cannot find (because they aren't registered).
+            "box2dweb", // TODO: Big, takes a lot of mem.
             "accounting.js", // TODO: NullPointerException in TAJS.
             "lunr.js", // can analyze. But plenty or errors in top-level constructors, meaning we skip a lot of tests.
             "PDF.js", // can analyze. But top-level constructor gets retracted.
             "Medium Editor", // TODO: Top level object not found.
             "Handlebars", // TODO: Error in top-level object.
             "Redux", // TODO: Top level object not found (try to not have an exports object)
-            "Knockout", // 157 violations in the top-level object. So no methods are analyzed.
             "axios", // TODO: Module not found (node?)
             "PeerJS", // TODO: Top level constructor always returns exceptionally.
             "Hammer.js", // TODO: Seemingly have some false positives (like Hammer.TouchAction.preventDefaults).
@@ -125,19 +126,24 @@ public class AnalyzeBenchmarks extends TestCase {
 
     @Test(timeout = (int)(BENCHMARK_TIMEOUT * 1000 * 1.3))
     public void analyzeBenchmarkPatched() throws Exception {
-        Path dtspath = Paths.get(this.benchmark.dTSFile);
-        Path entryPath = Paths.get(this.benchmark.jsFile);
-        String patched = dtspath.getParent().resolve("patched." + dtspath.getFileName()).toString();
-        if (!new File(patched).exists()) {
-            return;
-        }
-        String patchedEntry = entryPath.getParent().resolve("patched." + entryPath.getFileName()).toString();
-        Benchmark benchmark = this.benchmark.withOptions(options())
-                .withDecl(patched)
-                .withJsFile(patchedEntry);
+        Benchmark benchmark = getPatchedBenchmark(this.benchmark).withOptions(options());
         TAJSUtil.TajsAnalysisResults result = TAJSUtil.runNoDriver(benchmark, Integer.MAX_VALUE);
         System.out.println(result);
         assert(!result.timedout);
+    }
+
+    public static Benchmark getPatchedBenchmark(Benchmark benchmark) {
+        Path dtspath = Paths.get(benchmark.dTSFile);
+        Path entryPath = Paths.get(benchmark.jsFile);
+        String patched = dtspath.getParent().resolve("patched." + dtspath.getFileName()).toString();
+        if (!new File(patched).exists()) {
+            return null;
+        } else {
+            String patchedEntry = entryPath.getParent().resolve("patched." + entryPath.getFileName()).toString();
+            return benchmark
+                    .withDecl(patched)
+                    .withJsFile(patchedEntry);
+        }
     }
 
     @Test(timeout = (int)(INIT_TIMEOUT * 1000 * 1.3))
