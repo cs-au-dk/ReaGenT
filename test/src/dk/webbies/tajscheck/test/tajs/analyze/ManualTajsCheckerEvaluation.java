84a24664ffccbaf52917d8601dcaba3d60ca0898
Erik Krogh Kristensen <erik@webbies.dk>
2018-01-12 12:55:29 +0100

    Can actually run Manual evaluation in a loop. And .gitignore on the output files

--- test/src/dk/webbies/tajscheck/test/tajs/analyze/ManualTajsCheckerEvaluation.java
+++ test/src/dk/webbies/tajscheck/test/tajs/analyze/ManualTajsCheckerEvaluation.java
@@ -17,8 +17,44 @@ public class ManualTajsCheckerEvaluation {
 
     private static final int TIMEOUT = 20 * 60; // in seconds
 
+    private static final Set<String> CAN_DELTA_DEBUG = new HashSet<>(Arrays.asList(
+            "Hammer.js",
+            "PDF.js",
+            "PhotoSwipe",
+            "Swiper",
+            "pathjs",
+//            "Redux", // has a .d.ts file that imports another.
+            "Sortable",
+            "accounting.js",
+            "axios",
+            "lunr.js",
+
+            "reveal.js",
+            "box2dweb",
+            "QUnit",
+            "PleaseJS", // cheap
+            "Knockout", // cheap
+//            "RxJS", Has its actual types spread out all in many sub-files.
+            "async",
+            "Intro.js",
+            "CreateJS",
+            "Handlebars",
+            "highlight.js",
+            "bluebird",
+            "Medium Editor" // TAJS never terminates on the global constructor
+    ));
+
     @Test
-    public void doEval() throws Exception {
+    public void runInALoop() throws Exception {
+        List<String> benchmarks = TajsCheckerEvaluation.benchmarksToEvaluate;
+        //noinspection InfiniteLoopStatement
+        while (true) {
+            findATypeError(benchmarks.get(new Random().nextInt(benchmarks.size())));
+        }
+    }
+
+    @Test
+    public void doASingleEval() throws Exception {
         findATypeError("lunr.js");
     }
 
@@ -44,10 +80,44 @@ public class ManualTajsCheckerEvaluation {
         Collections.shuffle(typeViolations);
 
         TypeViolation typeViolation = typeViolations.iterator().next();
-        deltaDebugToFindViolation(bench, TypeViolation.definite(typeViolation.message, Util.simplifyPath(typeViolation.path)));
+        reportViolation(bench, TypeViolation.definite(typeViolation.message, Util.simplifyPath(typeViolation.path)));
     }
 
-    private void deltaDebugToFindViolation(Benchmark bench, TypeViolation searchViolation) throws IOException {
+    private void reportViolation(Benchmark bench, TypeViolation searchViolation) throws IOException {
+        new File("results").mkdir();
+        new File("results/manualEval").mkdir();
+
+        int counter = 0;
+        while (new File("results/manualEval/" + counter).exists()) {
+            counter++;
+        }
+
+        new File("results/manualEval/" + counter).mkdir();
+
+        Util.writeFile("results/manualEval/" + counter + "/" + bench.name + "_violation.txt", searchViolation.toString());
+
+        String dTsFile = Util.readFile(bench.dTSFile);
+        String jsFile = Util.readFile(bench.jsFile);
+
+        if (!CAN_DELTA_DEBUG.contains(bench.name)) {
+            return;
+        }
+
+        Util.writeFile("results/manualEval/" + counter + "/" + bench.name + ".d.ts.original", dTsFile);
+        Util.writeFile("results/manualEval/" + counter + "/" + bench.name + ".js.original", jsFile);
+
+
+        deltaDebugViolation(bench, searchViolation);
+
+        Util.writeFile("results/manualEval/" + counter + "/" + bench.name + ".d.ts", Util.readFile(bench.dTSFile));
+        Util.writeFile("results/manualEval/" + counter + "/" + bench.name + ".js", Util.readFile(bench.jsFile));
+
+
+        Util.writeFile(bench.dTSFile, dTsFile);
+        Util.writeFile(bench.jsFile, jsFile);
+    }
+
+    private void deltaDebugViolation(Benchmark bench, TypeViolation searchViolation) throws IOException {
         BooleanSupplier predicate = () -> {
             try {
                 TAJSUtil.TajsAnalysisResults result = TAJSUtil.runNoDriver(bench, TIMEOUT);
@@ -58,28 +128,8 @@ public class ManualTajsCheckerEvaluation {
                 return false;
             }
         };
-        String dTsFile = Util.readFile(bench.dTSFile);
-        String jsFile = Util.readFile(bench.jsFile);
 
         DeltaDebug.debug(bench.dTSFile, predicate);
         DeltaDebug.debug(bench.jsFile, predicate);
-
-        new File("manualEval").mkdir();
-
-        int counter = 0;
-        while (new File("manualEval/" + counter).exists()) {
-            counter++;
-        }
-
-        new File("manualEval/" + counter).mkdir();
-
-        Util.writeFile("manualEval/" + counter + "/" + bench.name + ".d.ts", Util.readFile(bench.dTSFile));
-        Util.writeFile("manualEval/" + counter + "/" + bench.name + ".d.ts.original", dTsFile);
-        Util.writeFile("manualEval/" + counter + "/" + bench.name + ".js", Util.readFile(bench.jsFile));
-        Util.writeFile("manualEval/" + counter + "/" + bench.name + ".js.original", jsFile);
-        Util.writeFile("manualEval/" + counter + "/violation.txt", searchViolation.toString());
-
-        Util.writeFile(bench.dTSFile, dTsFile);
-        Util.writeFile(bench.jsFile, jsFile);
     }
 }
