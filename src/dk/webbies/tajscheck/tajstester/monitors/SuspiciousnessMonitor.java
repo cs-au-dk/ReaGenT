860eaced6b664c95f1d44034254a7a67d14dedb9
Erik Krogh Kristensen <erik@webbies.dk>
2018-05-02 14:30:01 +0200

    Update suspisousness monitor

--- src/dk/webbies/tajscheck/tajstester/monitors/SuspiciousnessMonitor.java
+++ src/dk/webbies/tajscheck/tajstester/monitors/SuspiciousnessMonitor.java
@@ -6,9 +6,11 @@ import dk.brics.tajs.lattice.*;
 import dk.brics.tajs.monitoring.DefaultAnalysisMonitoring;
 import dk.brics.tajs.solver.NodeAndContext;
 import dk.webbies.tajscheck.tajstester.TajsTypeTester;
+import dk.webbies.tajscheck.tajstester.TesterContextSensitivity;
 import dk.webbies.tajscheck.testcreator.test.Test;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.function.BiConsumer;
 
@@ -36,33 +38,32 @@ public class SuspiciousnessMonitor extends DefaultAnalysisMonitoring {
 
     @Override
     public void visitNodeTransferPre(AbstractNode n, State s) {
-        this.currentBc = new NodeAndContext<Context>(n, s.getContext());
+        this.currentBc = new NodeAndContext<>(n, s.getContext());
     }
-
 
     @Override
     public void visitCall(AbstractNode n, Value funval) {
         if(currentBc != null) {
-            updateSuspicousnessInfo(currentBc, funval);
+            updateSuspicousnessInfo(currentBc, funval.getObjectLabels());
         }
     }
 
     @Override
     public void visitPropertyRead(AbstractNode n, Set<ObjectLabel> objs, PKeys propertystr, State state, boolean check_unknown) {
         if(currentBc != null && objs != null) {
-            updateSuspicousnessInfo(currentBc, Value.makeObject(objs));
+            updateSuspicousnessInfo(currentBc, objs);
         }
     }
 
     @Override
     public void visitPropertyWrite(Node n, Set<ObjectLabel> objs, PKeys propertystr) {
         if(currentBc != null && objs != null) {
-            updateSuspicousnessInfo(currentBc, Value.makeObject(objs));
+            updateSuspicousnessInfo(currentBc, objs);
         }
     }
 
-    private void updateSuspicousnessInfo(NodeAndContext<Context> nc, Value v) {
-        if(hasSuspiciousPrecisionLoss(v) && tester.getSensitivity() != null && tester.getSensitivity().isTestContext(nc.getContext())) {
+    private void updateSuspicousnessInfo(NodeAndContext<Context> nc, Set<ObjectLabel> objs) {
+        if(hasSuspiciousPrecisionLoss(objs) && tester.getSensitivity() != null && TesterContextSensitivity.isTestContext(nc.getContext())) {
             Test t = tester.getSensitivity().getTest(nc.getContext());
             suspiciousLocations.putIfAbsent(t, newSet());
             Set<NodeAndContext<Context>> snc = suspiciousLocations.get(t);
@@ -72,10 +73,10 @@ public class SuspiciousnessMonitor extends DefaultAnalysisMonitoring {
     }
 
 
-    private boolean hasSuspiciousPrecisionLoss(Value v) {
+    private boolean hasSuspiciousPrecisionLoss(Set<ObjectLabel> objs) {
         int native_functions = 0;
         int user_functions = 0;
-        for (ObjectLabel objlabel : v.getObjectLabels()) {
+        for (ObjectLabel objlabel : objs) {
             if (objlabel.getKind() == ObjectLabel.Kind.FUNCTION) {
                 if (objlabel.isHostObject()) {
                     native_functions++;
