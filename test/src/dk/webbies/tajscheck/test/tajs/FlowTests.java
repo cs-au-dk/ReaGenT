d503a1a705ee867e9c13f2e3f07bb20d2074d0ee
Erik Krogh Kristensen <erik@webbies.dk>
2018-06-19 10:06:29 +0200

    Move stuff

--- test/src/dk/webbies/tajscheck/test/tajs/FlowTests.java
+++ test/src/dk/webbies/tajscheck/test/tajs/FlowTests.java
@@ -68,7 +68,7 @@ public class FlowTests {
     }
 
     static Benchmark benchFromFolder(String folderName, OptionsI.Builder options, Benchmark.RUN_METHOD run_method) {
-        return new Benchmark("flow-" + folderName, ParseDeclaration.Environment.ES6DOM, "test/flowtyped/" + folderName + "/implementation.js", "test/flowtyped/" + folderName + "/declaration.js", run_method, options.build());
+        return new Benchmark("flow-" + folderName, ParseDeclaration.Environment.ES6DOM, "test/flowtests/" + folderName + "/implementation.js", "test/flowtests/" + folderName + "/declaration.js", run_method, options.build());
     }
 
     @Test
@@ -84,6 +84,11 @@ public class FlowTests {
     @Test
     public void parseUnified() {
         BenchmarkInfo.create(benchFromFolder("unified"));
+    }
+
+    @Test
+    public void parseUnderscoreDotString() {
+        BenchmarkInfo.create(benchFromFolder("underscore.string"));
     }
 
     @Test
