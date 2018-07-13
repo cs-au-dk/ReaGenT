aa0897e6d1865235c37b01d5a53860becf7f49a8
Erik Krogh Kristensen <erik@webbies.dk>
2018-07-13 13:19:56 +0200

    benign stuff

--- test/flowtyped/signals/declaration.js
+++ test/flowtyped/signals/declaration.js
@@ -3,10 +3,10 @@ declare module 'signals' {
         active: boolean;
         context: ?mixed;
         params: ?mixed[];
-        constructor(signal: Signal, listener: () => void, isOnce: boolean, listenerContext: ?mixed, priority: ?number): void;
-        detach(): ?() => void;
+        constructor(signal: Signal, listener: Function, isOnce: boolean, listenerContext: ?mixed, priority: ?number): void;
+        detach(): ?Function;
         execute(paramsArr: ?mixed[]): mixed;
-        getListener(): () => void;
+        getListener(): Function;
         getSignal(): Signal;
         isBound(): boolean;
         isOnce(): boolean;
@@ -17,15 +17,15 @@ declare module 'signals' {
         memorize: boolean;
         VERSION: string;
         constructor(): void;
-        add(listener: () => void, listenerContext: ?mixed, priority: ?number): SignalBinding;
-        addOnce(listener: () => void, listenerContext: ?mixed, priority: ?number): SignalBinding;
+        add(listener: Function, listenerContext: ?mixed, priority: ?number): SignalBinding;
+        addOnce(listener: Function, listenerContext: ?mixed, priority: ?number): SignalBinding;
         dispatch(...params: mixed[]): void;
         dispose(): void;
         forget(): void;
         getNumListeners(): number;
         halt(): void;
-        has(listener: () => void, context: ?mixed): boolean;
-        remove(listener: () => void, context: ?mixed): () => void;
+        has(listener: Function, context: ?mixed): boolean;
+        remove(listener: Function, context: ?mixed): () => void;
         removeAll(): void;
         toString(): string;
     }
