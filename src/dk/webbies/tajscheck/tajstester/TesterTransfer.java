86a38ae14ac0a2372c0871369d0e45de352c36e0
Erik Krogh Kristensen <erik@webbies.dk>
2019-01-16 14:07:32 +0100

    Remove unneeded state argument from EdgeTransfer

--- src/dk/webbies/tajscheck/tajstester/TesterTransfer.java
+++ src/dk/webbies/tajscheck/tajstester/TesterTransfer.java
@@ -1,17 +1,27 @@
 package dk.webbies.tajscheck.tajstester;
 
+import dk.brics.tajs.analysis.Solver;
 import dk.brics.tajs.analysis.Transfer;
 import dk.brics.tajs.flowgraph.AbstractNode;
 import dk.brics.tajs.flowgraph.BasicBlock;
 import dk.brics.tajs.flowgraph.jsnodes.EventDispatcherNode;
 import dk.brics.tajs.lattice.Context;
 import dk.brics.tajs.lattice.State;
-import dk.webbies.tajscheck.testcreator.test.Test;
 
 public class TesterTransfer extends Transfer {
+
+    private Solver.SolverInterface c;
+
     @Override
-    public Context transfer(BasicBlock src, BasicBlock dst, State state) {
+    public void setSolverInterface(Solver.SolverInterface c) {
+        super.setSolverInterface(c);
+        this.c = c;
+    }
+
+    @Override
+    public Context transfer(BasicBlock src, BasicBlock dst) {
         AbstractNode n = src.getLastNode();
+        State state = c.getState();
 
         if (n instanceof EventDispatcherNode && ((EventDispatcherNode) n).getType() == EventDispatcherNode.Type.TYPE_TESTS) {// we want flow for the "all-tests" context
             // But we also want to kill any test-context when moving outside of the testing block
@@ -19,6 +29,6 @@ public class TesterTransfer extends Transfer {
             return state.getContext();
         }
 
-        return super.transfer(src, dst, state);
+        return super.transfer(src, dst);
     }
 }
